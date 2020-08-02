7ffbbe9dcf16e50779376a128323ea7286830a62
Thomas <thomas-ln@laposte.net>
2020-08-02 18:31:17 +0200

    Updates

---
+++ precache-manifest.07cc559cbd67ab6ea31589a793403f11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b155b19f37fc841f7bfa06030bf6fad",
+    "url": "/react-countries/index.html"
+  },
+  {
+    "revision": "93c3b355624621b33f6c",
+    "url": "/react-countries/static/css/2.62154d37.chunk.css"
+  },
+  {
+    "revision": "3bb6389cb8a5dad57b22",
+    "url": "/react-countries/static/css/main.0c6651ee.chunk.css"
+  },
+  {
+    "revision": "93c3b355624621b33f6c",
+    "url": "/react-countries/static/js/2.44ae62f1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-countries/static/js/2.44ae62f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bb6389cb8a5dad57b22",
+    "url": "/react-countries/static/js/main.a3eaa914.chunk.js"
+  },
+  {
+    "revision": "862ba3a32a2fec2fba96",
+    "url": "/react-countries/static/js/runtime-main.4d95c3e6.js"
+  }
+]);
